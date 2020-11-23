2248f7f916036e3dda5ca7714e4c117e61e57235
Tippett <17506770+dtaivpp@users.noreply.github.com>
2020-11-23 16:58:14 -0500

    Created main file with threaded workers

---
+++ pdf-dex/main.py
@@ -0,0 +1,32 @@
+from concurrent.futures import ThreadPoolExecutor
+import threading
+import argparse
+
+from file_handling import get_pdf_list
+from tokenize import process_text
+
+def create_threaded_workers(MAX_THREADS):
+    """
+    Finds all the files in a directory and creates worker threads
+    """
+    # Get all the pdf paths
+    pdf_list = get_pdf_list()
+
+    # Create threadpool
+    t = ThreadPoolExecutor(max_workers=MAX_THREADS)
+    
+    # Process PDF's
+    t.map(process_text, pdf_list)
+    t.shutdown()
+
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser(description='Process PDF\'s into Elasticsearch Index')
+    parser.add_argument('threads', type=int,
+                        help='Number of threads to run process under.')
+
+
+    args = parser.parse_args()
+
+    create_threaded_workers(args.threads)
+
