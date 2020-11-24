975c65e8ffab00bb393073412d4c385a8d39f46d
David Tippett <17506770+dtaivpp@users.noreply.github.com>
2020-11-23 23:42:20 -0500

    Changed to use new pdf type

--- pdf-dex/file_handling.py
+++ pdf-dex/file_handling.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import PyPDF4
 import textract
-import 
+from utilities.types import PDF
 
 def read_elastic_metadata(file_name):
     pass
@@ -14,15 +14,17 @@ def get_pdf_list() -> list:
     return [ path.name for path in Path('data').rglob('*.pdf')]
 
 
-def read_pdf(file_path: str) -> str:
+def read_pdf(file_path: str) -> PDF:
     '''
     Open and reads pdf content
     '''
     full_text = ""
-    
+    metadata = None
+
     # Attempt to read with PyPDF4
     with open(file_path, 'rb') as f:
         pdf_reader = PyPDF4.PdfFileReader(f)
+        metadata = pdf_reader.getDocumentInfo()
 
         for page in pdf_reader.pages:
             full_text += page.extractText()
@@ -31,7 +33,7 @@ def read_pdf(file_path: str) -> str:
     if full_text == "":
         full_text = textract.process(fileurl, method='tesseract', language='eng')
 
-    return full_text
+    return PDF(file_path, full_text, metadata)
 
 
 if __name__=="__main__":
