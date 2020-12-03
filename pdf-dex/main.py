feab47b93cfe6e58012e2b761562069b417a55af
David Tippett <17506770+dtaivpp@users.noreply.github.com>
2020-12-02 23:57:44 -0500

    verifies the index exists othewise builds index

--- pdf-dex/main.py
+++ pdf-dex/main.py
@@ -1,7 +1,7 @@
 import os
-from concurrent.futures import ThreadPoolExecutor
-import threading
+from concurrent.futures import ThreadPoolExecutor, as_completed
 import argparse
+from elastic_connector import verify_indexes
 
 from file_handling import get_pdf_list
 from process_files import process_file
@@ -10,23 +10,25 @@ def create_threaded_workers(MAX_THREADS):
     """
     Finds all the files in a directory and creates worker threads
     """
+
+    verify_indexes()
+
     # Get all the pdf paths
     pdf_list = get_pdf_list()
 
     # Create threadpool
     t = ThreadPoolExecutor(max_workers=MAX_THREADS)
-    
-    # Process PDF's
     t.map(process_file, pdf_list)
-    t.shutdown()
 
+    t.shutdown(wait=True)
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser(description='Process PDF\'s into Elasticsearch Index')
     
-    parser.add_argument('threads', 
+    parser.add_argument('--threads', 
                         type=int,
-                        help='Number of threads to run process under.')
+                        help='Number of threads to run process under.',
+                        default=1)
 
     args = parser.parse_args()
 
